1d68b3e9fd70539c6ee024c005cf97b818f1aca3
Felipe Ricardi <felipe.ricardi@sap.com>
2020-10-04 18:37:16 -0300

    Updating crc8 implementation

--- src/ecc/impl/Crc8.java
+++ src/ecc/impl/Crc8.java
@@ -3,6 +3,8 @@ package ecc.impl;
 import ecc.Crc;
 import utility.StringUtils;
 
+import java.io.ByteArrayOutputStream;
+
 public class Crc8 implements Crc {
     private static final int CRC8CODE = 263;
 
@@ -14,7 +16,7 @@ public class Crc8 implements Crc {
         bytes = bytes.concat(StringUtils.createStringSequence("0", 8));
         int sum = 0;
         int needs = code.length();
-        while(needs < bytes.length()) {
+        while(needs <= bytes.length()) {
             int firstOneIndex = bytes.indexOf('1');
             String usingBytes = bytes.substring(firstOneIndex, firstOneIndex + needs);
             bytes = bytes.substring(firstOneIndex + needs);
@@ -23,13 +25,19 @@ public class Crc8 implements Crc {
             bytes = Integer.toBinaryString(sum & 0xff).concat(bytes);
         }
         String sumString = Integer.toBinaryString(sum);
-        String finalSum = sumString.length() == 8 ? sumString : sumString.concat(StringUtils.createStringSequence("0", 8 - sumString.length()));
-
-        return (byte) Integer.parseInt(finalSum);
+        String finalSum = sumString.length() == 8 ? sumString : StringUtils.createStringSequence("0", 8 - sumString.length()).concat(sumString);
+        int finalSumByte = Integer.parseInt(finalSum, 2);
+        return (byte) (finalSumByte < 0 ? finalSumByte + 256 : finalSumByte) ;
     }
 
     @Override
     public boolean checksum(byte[] buffer) {
-        return false;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        for(int i = 0; i < buffer.length - 1; i++) {
+            baos.write(buffer[i]);
+        }
+
+
+        return buffer[buffer.length-1] == checksumResult(baos.toByteArray());
     }
 }
