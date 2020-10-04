df7ca295dc59a0991f459b22603786f8c70ec368
Felipe Ricardi <felipe.ricardi@sap.com>
2020-10-04 01:33:59 -0300

    Updating encoder abstratct class

--- src/encodings/Encoder.java
+++ src/encodings/Encoder.java
@@ -1,31 +1,33 @@
 package encodings;
 
+import utility.StringUtils;
+
 import java.io.ByteArrayOutputStream;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class Encoder {
 
+    public int getDivider() {
+        return 0;
+    }
+
+    abstract public int getCode();
+
     public byte[] encodeText(String text) {
         List<Integer> asciiValue = text.chars().mapToObj(c -> (char) c).map(character -> (int) character).collect(Collectors.toList());
 
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
         List<String> encodedChars = asciiValue.stream().map(this::encodeNumber).collect(Collectors.toList());
-
+        encodedChars.add(0, StringUtils.getByteFromNumber(this.getCode()));
+        encodedChars.add(1, StringUtils.getByteFromNumber(this.getDivider()));
         return byteStringListToByteArray(encodedChars);
     }
 
-    private byte[] byteStringListToByteArray(List<String> byteStringList) {
+    protected byte[] byteStringListToByteArray(List<String> byteStringList) {
         String joinedString = byteStringList.stream().reduce(String::concat).get();
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 
         String[] str = joinedString.split("(?<=\\G.{8})");
-        List<Byte> bytes = new ArrayList<>();
 
         for(String cur : str) {
             byte b = 0b00000000;
